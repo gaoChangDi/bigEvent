bce93caaba03c2b9b94e287a035af7ea7ca8780e
gcd <2321721643@qq.com>
2021-07-18 16:31:25 +0800

    init user

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,6 @@
 $(function() {
     getUserInfo();
 
-    function getUserInfo() {
-        $.ajax({
-            method: 'GET',
-            url: '/my/userinfo',
-            success: function(res) {
-                if (res.status !== 0) {
-                    return layer.msg('获取用户信息失败')
-                } else {
-                    renderAvatar(res.data)
-                }
-            }
-        })
-    };
-
-    function renderAvatar(user) {
-        var name = user.nickname || user.username;
-        $('#welcome').html('欢迎&nbsp&nbsp' + name);
-        if (user.user_pic !== null) {
-            $('.layui-nav-img').attr('src', user.user_pic).show();
-            $('.text-avatar').hide()
-        } else {
-            $('.layui-nav-img').hide();
-            var first = name[0].toUpperCase();
-            $('.text-avatar').html(first).show()
-        }
-    };
-
     $('#btnLogout').on('click', function() {
         // console.log(1);
         layui.layer.confirm('确定退出登录？', {
@@ -42,14 +15,32 @@ $(function() {
         )
     })
 
-
-
-
-
-
-
-
-
-
-
 })
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status !== 0) {
+                return layer.msg('获取用户信息失败')
+            } else {
+                renderAvatar(res.data)
+            }
+        }
+    })
+};
+
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp&nbsp' + name);
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show()
+    }
+};
